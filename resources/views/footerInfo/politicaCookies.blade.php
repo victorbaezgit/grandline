192c46d4412f365d55d5433b3a3b083a3c95f2c7
victorbaezgit <victorbaezgit@gmail.com>
2023-06-14 11:39:33 +0200

    cambios infofooter

--- resources/views/footerInfo/politicaCookies.blade.php
+++ resources/views/footerInfo/politicaCookies.blade.php
@@ -6,8 +6,7 @@
     <p>El Sitio Web utiliza cookies. Una cookie es un archivo de texto que se almacena en su dispositivo cuando usted visita una página web.</p>
     <p>El Sitio Web emplea cookies de terceros persistentes con fines analíticos, para distinguir usuarios y sesiones, saber el tiempo que pasa el usuario en el Sitio Web, el número de páginas que visita, así como desde dónde y cómo ha llegado el usuario al Sitio Web.</p>
     <p>Las cookies de terceros corresponden al servicio de Google Analytics ofrecido por Google Inc. (1600 Amphitheatre Pkwy, Mountain View, CA 94043, Estados Unidos). Puede encontrar más información acerca de las condiciones de uso y privacidad de datos en
-        <a href="http://www.google.com/analytics/terms/es.html">http://www.google.com/analytics/terms/es.html</a>  y en
-        <a href="http://www.google.com/intl/es/analytics/privacyoverview.html">http://www.google.com/intl/es/analytics/privacyoverview.html</a>.</p>
+        <a href="http://www.google.com/analytics/terms/es.html">http://www.google.com/analytics/terms/es.html</a>.</p>
     <p>El usuario puede evitar que se transmitan a Google de los datos generados por las cookies de Google Analytics y referentes a su uso del Sitio Web descargando e instalando el plugin disponible en el siguiente enlace: http://tools.google.com/dlpage/gaoptout?hl=es.</p>
     <p>Asimismo, el usuario puede rechazar el uso de cookies mediante la selección de la configuración apropiada de su navegador. Sin embargo, tenga en cuenta que si lo hace puede que no pueda usar el Sitio Web en su plena funcionalidad.</p>
     <p>La eliminación de las cookies depende del navegador que esté utilizando. A continuación, puede usted encontrar enlaces a las instrucciones para eliminar cookies que ofrecen los principales navegadores:</p>
